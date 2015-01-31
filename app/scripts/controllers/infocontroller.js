ad5a18c54a1636a8b84e5519805d605fb47ff4e5
newville23 <villanuevad@uninorte.edu.co>
2015-01-31 10:05:48 -0500

    added add and remove button for ocompras

--- app/scripts/controllers/infocontroller.js
+++ app/scripts/controllers/infocontroller.js
@@ -8,11 +8,36 @@
  * Controller of the yoAngularApp
  */
 angular.module('yoAngularApp')
-  .controller('InfoCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    
+  .controller('InfoCtrl', function ($scope, $log, $firebase) {
+		    //sincronizacion de array de materiales
+		var refmateriales = new Firebase('https://solutionsapp.firebaseio.com/materiales');
+
+		var sync = $firebase(refmateriales);
+
+		$scope.materiales = sync.$asArray();
+
+		//Sincronizacion de array de proveedores 
+		var refProveedores = new Firebase('https://solutionsapp.firebaseio.com/proveedor');
+
+		var syncpro = $firebase(refProveedores);
+
+		$scope.proveedores = syncpro.$asArray();
+
+		//variable de conteo de item
+		var citem = 1;
+
+		$scope.items = [];
+		 
+		 $scope.addItem = function() {
+		 	
+		   	$scope.items.push({item: citem, descripcion:'', cantidad: '0', vunit:'', total:'' });
+		   	citem++;
+		 };
+		 
+		 $scope.removeItem = function() {
+		    $scope.items.pop();
+		 };
+
+		    
   });
+		
