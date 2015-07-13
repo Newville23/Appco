85accf9f73ed0b2d60e3d62894af10db2ba4f987
newville23 <villanuevad@uninorte.edu.co>
2015-07-13 17:37:04 -0500

    added newoc dialog and total

--- app/scripts/controllers/infocontroller.js
+++ app/scripts/controllers/infocontroller.js
@@ -9,21 +9,29 @@
  */
 angular.module('yoAngularApp')
   .controller('InfoCtrl', function ($scope, $log, $firebase) {
-		    //sincronizacion de array de materiales
+	//sincronizacion de array de ordenes de compra
+
+		var refOc = new Firebase('https://solutionsapp.firebaseio.com/Ocs');
+
+		var syncOc = $firebase(refOc);
+
+		$scope.Ocs = sync.$asArray();
+
+	 //sincronizacion de array de materiales
 		var refmateriales = new Firebase('https://solutionsapp.firebaseio.com/materiales');
 
 		var sync = $firebase(refmateriales);
 
 		$scope.materiales = sync.$asArray();
 
-		//Sincronizacion de array de proveedores 
+	//Sincronizacion de array de proveedores 
 		var refProveedores = new Firebase('https://solutionsapp.firebaseio.com/proveedor');
 
 		var syncpro = $firebase(refProveedores);
 
 		$scope.proveedores = syncpro.$asArray();
 
-		//variable de conteo de items
+	//variable de conteo de items
 		var count = 1;
 
 		$scope.items = [];
@@ -37,30 +45,38 @@ angular.module('yoAngularApp')
 		 
 		 $scope.addItem = function() {
 		 	
-		   	$scope.items.push({item: count, descripcion:'', cant: '1', vunit:'', total:'' });
+		   	$scope.items.push({item: count, descripcion:'', cant: '1'});
 		   	count = count + 1;
 		 };
 		 
-		 $scope.upitem = function(it){
-		 	console.log("Aqui estoy");
-		 	$scope.item.vunit = it.descripcion.vunit ;
+		 $scope.showAdvanced = function(ev) {
+		 	$mdDialog.show({
+		 		controller: DialogMaterial,
+		 		templateUrl: 'views/dialog/newOc.html',
+		 		targetEvent: ev,
+		 	})
+		 	.then(function(oc) {
 
+		 		$scope.Oc.$add({ id: "1", items: oc.items, fecha: , subtotal:'', iva:'', total: oc.total()});
+		 	});
 		 };
 
-		 //remove the last item from the list of materiales in the OC
+		//remove the last item from the list of materiales in the OC
 
 		 $scope.removeItem = function() {
 		    $scope.items.pop();
 		    count = count -1;
 		 };
 
-		$scope.getTotal = function() {
-        var total = 0;
-        angular.forEach($scope.items, function(item) {
-            total += item.cant * item.vunit;
-        });
+		$scope.total = function() {
+	        var total = 0;
+	        angular.forEach($scope.items, function(item) {
+	        total += item.cant * item.descripcion.vunit;
 
-        return total;
+        });
+	       
+	       
+        	return total;
     };
 
   });
